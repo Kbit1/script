ff9aeb9e4495c301976d6a68bfb40b2aea1f9a38
Kbit1 <cjase@naver.com>
2025-01-28 00:26:06 +0900

    Update motion.js

--- motion.js
+++ motion.js
@@ -1,25 +1,25 @@
 $(function(){
 
-console.log("실행확인")
 
 $("#btn").on("click",function(e){
-
-    let 어레이 = [1,2,3];
-    어레이.forEach(function(a){
+    let array = [1,2,3];
+    array.forEach(function(a){
         console.log(this)
     });
+
 })
 
 
-let 오브젝트 = {
-    이름:["김","이","박"], 함수 :function(){
-
-        오브젝트.이름.forEach(()=>{
-            console.log(this)
+let object = {
+    name : ["Lee","Kim","Park"] , 
+    math : function(){
+        // console.log(this);
+        object.name.forEach(()=>{
+            console.log(this);
         })
     }
 }
-오브젝트.함수();
 
-});
-this 키워드를 알아보자 2. event listener와 constructor
+object.math()
+
+})
